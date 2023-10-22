888b223be8b1b8abf7ea0ecb928ce763ceae8787
Dineshjnld <dineshjnld22@gmail.com>
2023-10-22 17:29:04 +0530

    Initial commit

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import {Button, Callout, TextField} from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import {useForm,Controller } from 'react-hook-form';
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+import  { useState} from 'react'
+import {zodResolver} from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import {z} from 'zod';
+import IssuesPage from '../page';
+
+type IssueForm =z.infer<typeof createIssueSchema>;
+const NewIssuePage = () => {
+    const router= useRouter(); 
+const {register,control,handleSubmit,formState:{errors}}= useForm<IssueForm>({
+    resolver:zodResolver(createIssueSchema)
+});
+const [error,setError ]=useState('');
+return (
+    <div className="max-w-xl">
+        {error && (
+        <Callout.Root color="red" className='mb-5'>
+        <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>)
+}
+    <form 
+    className='max-w-xl space-y-3'
+    onSubmit={handleSubmit(async(data)=>
+    {
+        try{await axios.post('/api/issues',data);
+        router.push('/issues');} catch(error){
+            console.log(error)
+        }
+        
+    })} >
+        <TextField.Root>
+            <TextField.Input placeholder='Title' {...register('title')}/>
+        </TextField.Root>
+    
+        <Controller
+        name="description"
+        control={control}
+        render={(field)=><SimpleMDE placeholder='Description'/> }
+        />
+        
+        <Button>Submit New Issue</Button>
+        </form ></div>
+)
+}
+
+export default NewIssuePage
